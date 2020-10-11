5509b18aebf5a88a1515300899f0dac250b32801
Thura Sitt Naing <coding2am@gmail.com>
2020-10-11 10:11:08 +0630

    Fixing responsive and adding partnership

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
     $('.iconAnimate').hover(
         function(){ $(this).addClass('fa-spin') },
         function(){ $(this).removeClass('fa-spin') }
-    )
+    );
 });
